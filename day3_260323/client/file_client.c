f3078bf0247a4b9985e3e43823dd7b5eb47dcb9c
felix6631 <second6630@gmail.com>
2026-03-29 18:51:21 +0900

    day3 homework finished

--- day3_260323/client/file_client.c
+++ day3_260323/client/file_client.c
@@ -1,3 +1,4 @@
+//2023011844 Kim HyeonJin
 #include <stdio.h>
 #include <fcntl.h>
 #include <arpa/inet.h>
@@ -13,10 +14,11 @@ typedef struct {
   int buf_len;
   char buf[BUF_SIZE+1];
 } Packet;
-void handler(char[]);
+void handler(const char[]);
 int main(int argc, char *argv[]) {
   if(argc < 3)
-    handler("invalid param: halt\n");
+    handler("invalid param: ./file_client [Address] [Port]\n");
+  
   
   int sock;
   struct sockaddr_in addr_serv;
@@ -25,6 +27,10 @@ int main(int argc, char *argv[]) {
   sock = socket(PF_INET,SOCK_STREAM,0);
   if(sock == -1)
     handler("socket init failed.\n");
+#ifdef DEBUG
+  puts("socket initialized.");
+#endif
+
   memset(&addr_serv,0,sizeof(addr_serv));
   addr_serv.sin_family=AF_INET;
   addr_serv.sin_addr.s_addr=inet_addr(argv[1]);
@@ -34,42 +40,71 @@ int main(int argc, char *argv[]) {
   if(connect(sock,(struct sockaddr*)&addr_serv,sizeof(addr_serv))==-1)
     handler("connection failed.\n");
 
+#ifdef DEBUG
+  puts("server connected");
+#endif
+
   // TCP
   char filename[BUF_SIZE+1] = {0,};
   printf("Input file name: ");
   fgets(filename,BUF_SIZE,stdin);
+  if(filename[strlen(filename)-1] == '\n')
+    filename[strlen(filename)-1] = '\0';
+
   
-  Packet rdpack = {0,0,0,""};
-  Packet wrpack = {0,0,0,""};
+  Packet rdpack = {0,0,0,""}; memset(&rdpack,0,sizeof(rdpack));
+  Packet wrpack = {0,0,0,""}; memset(&wrpack,0,sizeof(wrpack));
 
   printf("[Client] request %s\n\n",filename);
   strcpy(wrpack.buf,filename);
+  wrpack.buf_len = strlen(wrpack.buf);
+
   write(sock,&wrpack,sizeof(wrpack));
-  read(sock,&rdpack,sizeof(rdpack));
+#ifdef DEBUG
+  puts("initial packet sent");
+  printf("wrpack: %d %d %d %s\n",wrpack.seq,wrpack.ack,wrpack.buf_len,wrpack.buf);
+#endif
+
+  read(sock,(Packet*)&rdpack,sizeof(rdpack));
+#ifdef DEBUG
+  puts("initial packet recieved");
+  printf("rdpack: %d %d %d %s\n",rdpack.seq,rdpack.ack,rdpack.buf_len,rdpack.buf);
+#endif
   if(strcmp(rdpack.buf,"\033[31mFile Not Found\033[0m")==0) {
-    handler("\033[31mFile Not Found\033[0m");
+    handler("\033[31mFile Not Found\033[0m\n");
   }
   int recv_bytes = 0;
-  int file = open(filename,O_CREAT|O_WRONLY|O_TRUNC);
-  while(read(sock,rdpack.buf,sizeof(rdpack))) {
+
+  int file = open(filename,O_CREAT|O_TRUNC|O_WRONLY,0644);
+  while(1) {
     printf("[Client] Rx SEQ: %d, len: %d bytes\n",rdpack.seq,rdpack.buf_len);
+    write(file, rdpack.buf, rdpack.buf_len);
+    memset(rdpack.buf,0,BUF_SIZE);
+    
     wrpack.ack = rdpack.seq + rdpack.buf_len + 1;
     recv_bytes += rdpack.buf_len;
     if(rdpack.buf_len < BUF_SIZE)
       break;
-    write(sock,&wrpack,sizeof(wrpack));
+
+    //ACK
+    write(sock,(Packet*)&wrpack,sizeof(wrpack));
     printf("[Client] Tx ACK: %d\n",wrpack.ack);
+
     
+    if(read(sock,(Packet*)&rdpack,sizeof(rdpack))==-1)
+      break;
+
+    puts("");
   }
-  printf("%s recieved (\033[31m%d bytes\033[0m)",filename,recv_bytes);
+  printf("%s recieved (\033[31m %d bytes\033[0m ) \n",filename,recv_bytes);
   
   // close
   printf("Exit client\n");
   close(sock);
   return 0;
 }
-void handler(char message[]) {
-  fputs(stderr,stderr);
-  printf("Exit client");
+void handler(const char message[]) {
+  fputs(message,stderr);
+  printf("Exit client\n");
   exit(1);
 }
--- day3_260323/server/file_server.c
+++ day3_260323/server/file_server.c
@@ -1,3 +1,4 @@
+//2023011844 Kim HyeonJin
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/socket.h>
@@ -16,7 +17,7 @@ typedef struct {
 void handler(char[]);
 int main(int argc, char *argv[]) {
   if(argc < 2)
-    handler("invalid param: halt\n");
+    handler("invalid param: ./file_server [port]\n");
   
   int sock_serv;
   int sock_clnt;
@@ -34,13 +35,30 @@ int main(int argc, char *argv[]) {
   addr_serv.sin_addr.s_addr=htonl(INADDR_ANY);
   addr_serv.sin_port = htons(atoi(argv[1]));
 
+#ifdef DEBUG
+  puts("socket initialized");
+#endif
+
   // step 2. bind
   if(bind(sock_serv, (struct sockaddr*)&addr_serv,sizeof(addr_serv))==-1)
     handler( "socket address bind failed.\n");
+
+#ifdef DEBUG
+  puts("socket address binded");
+#endif
   
   // step 3. listen
   if(listen(sock_serv, 5) == -1)
     handler("listen() failed.\n");
+
+#ifdef DEBUG
+  puts("socket opened; waiting for client...");
+#endif
+
+  printf("------------------------------\n");
+  printf("   File Transmission Server\n");
+  printf("------------------------------\n");
+
 
   addr_clnt_sz = sizeof(addr_clnt);
   
@@ -49,39 +67,66 @@ int main(int argc, char *argv[]) {
   if(sock_clnt == -1)
     handler("socket client accept failed.\n");
 
+#ifdef DEBUG
+  puts("clinet accepted");
+#endif
   // TCP
-  printf("------------------------------\n");
-  printf("   File Transmission Server\n");
-  printf("------------------------------\n");
+  
 
-  Packet rdpack = {0,0,0,""};
-  Packet wrpack = {0,0,0,""};
+  Packet rdpack = {0,0,0,""}; memset(&rdpack,0,sizeof(rdpack));
+  Packet wrpack = {0,0,0,""}; memset(&wrpack,0,sizeof(wrpack));
   read(sock_clnt,&rdpack,sizeof(rdpack));
   char filename[BUF_SIZE];
   strcpy(filename,rdpack.buf);
+  // strip
+  if(filename[strlen(filename)-1] == '\n')
+    filename[strlen(filename)-1] = '\0';
+
+  // find file and ENOENT exeption 
   int file = open(filename,O_RDONLY);
   if (file == -1) {
-    char msg[BUF_SIZE+1];
+    char msg[BUF_SIZE*2];
     sprintf(msg,"\033[31m%s File Not Found.\033[0m\n",filename);
     strcpy(wrpack.buf, "\033[31mFile Not Found\033[0m");
     wrpack.buf_len = strlen(wrpack.buf);
     write(sock_clnt,&wrpack,sizeof(wrpack));
     handler(msg);
   }
+  char fileInputBuf[BUF_SIZE]; memset(fileInputBuf,0,BUF_SIZE);
+  
+  //TCP
   printf("[Server] sending %s\n\n",rdpack.buf);
   wrpack.seq = 1000;
-  while(read(file,wrpack.buf,BUF_SIZE)!=-1) {
-    wrpack.buf_len = strlen(wrpack.buf);
+  while(1) { //reading from file
+    memset(fileInputBuf,0,BUF_SIZE);
+    int fileReadResult = read(file,fileInputBuf,BUF_SIZE);
+  #ifdef DEBUG
+    if(wrpack.seq > 5000) {
+      char dummy;
+      fflush(stdin);
+      scanf("%c",&dummy);
+    }
+    printf("read %d: %s\n",fileReadResult,fileInputBuf);
+  #endif
+  
+    strcpy(wrpack.buf,fileInputBuf);
+    wrpack.buf_len = strlen(fileInputBuf);
+    write(sock_clnt,(Packet*)&wrpack,sizeof(wrpack));
     printf("[Server] Tx: SEQ: %d, %d byte data\n",wrpack.seq,wrpack.buf_len);
+    
     send_bytes += wrpack.buf_len;
-    if(wrpack.buf_len < 100)
+    if(wrpack.buf_len < 100 || fileReadResult < 0)
       break;
-    read(sock_clnt,&rdpack,sizeof(rdpack));
+    read(sock_clnt,(Packet*)&rdpack,sizeof(rdpack));
     printf("[Server] Rx ACK: %d\n",rdpack.ack);
+    if(rdpack.ack == 0) {
+      printf("Client disconected;\n");
+      break;
+    }
     wrpack.seq = rdpack.ack;
     puts("");
   }
-  printf("%s sent (\033[31m %d bytes\033[0m)",filename,send_bytes);
+  printf("%s sent (\033[31m %d bytes\033[0m )\n",filename,send_bytes);
 
     
   // close
@@ -92,6 +137,6 @@ int main(int argc, char *argv[]) {
 }
 void handler(char message[]) {
   fputs(message,stderr);
-  printf("Exit server");
+  printf("Exit server\n");
   exit(1);
 }
